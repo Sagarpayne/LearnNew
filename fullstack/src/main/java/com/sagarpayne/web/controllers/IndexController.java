ac1dfbe0729009ddde407bb16e28277324a75fce
sagarpayne <Sagarpayne@gmail.com>
2017-07-30 17:16:58 +0530

    logback

--- fullstack/src/main/java/com/sagarpayne/web/controllers/IndexController.java
+++ fullstack/src/main/java/com/sagarpayne/web/controllers/IndexController.java
@@ -1,14 +1,19 @@
 package com.sagarpayne.web.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 	
+	private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
+	
 	@RequestMapping("/")
 	public String home()
 	{
+		LOG.info("What is up");
 		return "index";
 	}
 
