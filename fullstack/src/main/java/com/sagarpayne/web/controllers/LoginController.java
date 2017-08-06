2427f5719cdb65388985c1f256d8689a3210d1c5
sagarpayne <Sagarpayne@gmail.com>
2017-08-06 10:22:15 +0530

    login 1

---
+++ fullstack/src/main/java/com/sagarpayne/web/controllers/LoginController.java
@@ -0,0 +1,9 @@
+package com.sagarpayne.web.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+	public static final String LOGIN_VIEW_NAME = "user/login";
+}
